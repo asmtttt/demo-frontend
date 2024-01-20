b80c7a96539aab84cbde54ed094db16207236dd3
Samet <abdulsamet.ocsoy@gmail.com>
2024-01-20 16:34:47 +0300

    ISS #5 | Register design and development completed part1

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RegisterModel } from '../../models/register-request-model';
+
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerModel: RegisterModel = { 
+      usagePurpose: '',  
+      usageType: '', 
+      firstName: '', 
+      lastName: '',
+      email: '',
+      phone: '',
+      password: '',
+      passwordAgain: '',
+      isUsageRulesApproved: false,
+      isContractApproved: false};
+
+  onRegister(){
+    console.log("register model: ", this.registerModel);
+    
+  }
+
+  onUsageRulesCheckboxChange() {
+    this.registerModel.isUsageRulesApproved = !this.registerModel.isUsageRulesApproved;
+    console.log("Usage Rules checkbox changed: ", this.registerModel.isUsageRulesApproved);
+  }
+
+  onContractCheckboxChange() {
+    this.registerModel.isContractApproved = !this.registerModel.isContractApproved;
+    console.log("Contract checkbox changed: ", this.registerModel.isContractApproved);
+  }
 }
---
+++ src/app/features/auth/models/register-request-model.ts
@@ -0,0 +1,16 @@
+export interface RegisterModel {
+    usagePurpose: string;
+    usageType: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    password: string;
+    passwordAgain: string;
+    isUsageRulesApproved: boolean;
+    isContractApproved: boolean;
+    roleType?: string;
+    birthDate?: string;
+    countryCode?: string;
+    gsm?: string;
+  }
